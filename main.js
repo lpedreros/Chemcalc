71cf7b0f0c27cc6f0859a9a64f65701bce954b6e
MrLeho <lpedreros@gmail.com>
2025-03-18 13:39:13 -0400

    removed script tags

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-<script>
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
   var menuToggle = document.querySelector('.menu-toggle');
   var nav = document.getElementById('mainNav');
   menuToggle.addEventListener('click', function() {
     nav.classList.toggle('active');
   });
 });
-</script>// JavaScript Document
